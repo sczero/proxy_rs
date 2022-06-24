4e85558a9125407314cfc5a9c971cff613821411
宋诚中 <chengzhongsong@gmail.com>
2022-06-24 17:08:30 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     loop {
         let (mut in_socket, _) = listener.accept().await?;
         tokio::spawn(async move {
-            let mut byte_buf = BytesMut::with_capacity(4096);
+            let mut byte_buf = BytesMut::with_capacity(0);
             loop {
                 let mut byte_tmp = [0u8; 1024];
                 match in_socket.read(&mut byte_tmp).await {
@@ -32,7 +32,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
                         byte_buf.put_slice(&byte_tmp[..n]);
                         let byte_buf_ref = byte_buf.as_ref();
                         //结束
-                        if &byte_buf_ref[byte_buf_ref.len() - 4..] == b"\r\n\r\n" {
+                        if byte_buf_ref.len() >= 4
+                            && (&byte_buf_ref[byte_buf_ref.len() - 4..] == b"\r\n\r\n")
+                        {
                             break;
                         }
                     }
@@ -66,8 +68,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
             }
             println!("{:?}", header);
             if !header.line.is_https {
-                let url = Url::parse(&*header.line.url).unwrap();
-                let mut out_socket = tokio::net::TcpStream::connect(format!(
+                let url = Url::parse(&header.line.url).unwrap();
+                let mut out_socket = TcpStream::connect(format!(
                     "{}:{}",
                     url.domain().unwrap(),
                     url.port().unwrap_or(80)
@@ -76,8 +78,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
                 .unwrap();
                 let (mut out_reader, mut out_writer) = out_socket.into_split();
                 let (mut in_reader, mut in_writer) = in_socket.into_split();
-                out_writer.write_all(header_content.as_ref()).await.unwrap();
                 tokio::spawn(async move {
+                    out_writer.write_all(header_content.as_ref()).await.unwrap();
                     tokio::io::copy(&mut in_reader, &mut out_writer)
                         .await
                         .unwrap();
@@ -86,6 +88,26 @@ async fn main() -> Result<(), Box<dyn Error>> {
                     tokio::io::copy(&mut out_reader, &mut in_writer)
                         .await
                         .unwrap();
+                });
+            } else {
+                let mut out_socket = TcpStream::connect(header.line.url)
+                    .await
+                    .expect("connect out error!");
+                let (mut out_reader, mut out_writer) = out_socket.into_split();
+                let (mut in_reader, mut in_writer) = in_socket.into_split();
+                tokio::spawn(async move {
+                    tokio::io::copy(&mut in_reader, &mut out_writer)
+                        .await
+                        .expect("in >>> proxy >>> out error!");
+                });
+                tokio::spawn(async move {
+                    in_writer
+                        .write_all(b"HTTP/1.1 200 OK\r\n\r\n")
+                        .await
+                        .expect("200 OK >>>> in_writer error!");
+                    tokio::io::copy(&mut out_reader, &mut in_writer)
+                        .await
+                        .expect("in <<< proxy <<< out error!");
                 });
             }
         });
